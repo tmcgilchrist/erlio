15cf3102bd7de3865f3a6c647c285b736a8a60dc
Tim McGilchrist <timmcgil@gmail.com>
2012-07-04 08:03:14 +1000

    Initial project setup.

---
+++ README.md
@@ -0,0 +1,27 @@
+# erlio #
+
+Erlang url shortener project.
+
+## Goals ##
+ * webmachine application
+ * eunit tests included
+ * upgradable via hot code loading
+ * web interface ???
+ * gather stats ??
+
+## Author(s) ##
+
+ * Tim McGilchrist <timmcgil@gmail.com>
+
+## Copyright ##
+
+Copyright (c) 2012 Tim McGilchrist <timmcgil@gmail.com>.  All rights reserved.
+
+
+### TODO ###
+
+ * Generate basic webmachine project that compiles and starts
+ * Add url resource that takes JSON and prints out the values.
+ * Add mnesia to application
+ * Save url into mnesia
+ * Integration test using common_test
---
+++ src/erlio_sup.erl
@@ -0,0 +1,38 @@
+
+-module(erlio_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    Ip =  "0.0.0.0",
+    {ok, Dispatch} = file:consult(filename:join(
+                                    [filename:dirname(code:which(?MODULE)),
+                                     "..", "priv", "dispatch.conf"])),
+    WebConfig = [ {ip, Ip},
+                  {port, 8000},
+                  {log_dir, "priv/log"},
+                  {dispatch, Dispatch}],
+    Web = {webmachine_mochiweb,
+           {webmachine_mochiweb, start, [WebConfig]},
+           permanent, 5000, worker, [mochiweb_socket_server]},
+    {ok, { {one_for_one, 5, 10}, [Web]} }.
