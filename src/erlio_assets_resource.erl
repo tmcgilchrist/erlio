477056a31e94090de958319167c59787a49aead5
Tim McGilchrist <timmcgil@gmail.com>
2014-05-01 08:12:12 +1000

    Serve static assets like html/css/js.

---
+++ src/erlio_assets_resource.erl
@@ -0,0 +1,141 @@
+-module(erlio_assets_resource).
+
+-export([init/1,
+         routes/0,
+         to_resource/2,
+         allowed_methods/2,
+         generate_etag/2,
+         last_modified/2,
+         resource_exists/2,
+         content_types_provided/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+-include_lib("kernel/include/file.hrl").
+
+-record(context, {filename,
+                  fileinfo,
+                  rendered,
+                  token}).
+
+%% @doc Initialize the resource.
+-spec init([]) -> {ok, #context{}}.
+init([]) ->
+    {ok, #context{}}.
+
+%% @doc Return the routes this module should respond to.
+-spec routes() -> [webmachine_dispatcher:matchterm()].
+routes() ->
+    [
+     {["javascripts"], ?MODULE, []},
+     {["stylesheets"], ?MODULE, []},
+     {['*'], ?MODULE, []}
+    ].
+
+%% @doc Handle serving of the single page application.
+-spec allowed_methods(wrq:reqdata(), #context{}) ->
+    {list(), wrq:reqdata(), #context{}}.
+allowed_methods(ReqData, Context) ->
+    {['HEAD', 'GET'], ReqData, Context}.
+
+%% @doc Generates an etag for the asset being served.
+-spec generate_etag(wrq:reqdata(), #context{}) ->
+                           {list(), wrq:reqdata(), #context{}}.
+generate_etag(ReqData, #context{filename=template,
+                                rendered=Rendered}=Context) ->
+    {mochihex:to_hex(erlang:phash2(Rendered)), ReqData, Context};
+generate_etag(ReqData, #context{fileinfo=FileInfo}=Context) ->
+    {mochihex:to_hex(erlang:phash2(FileInfo)), ReqData, Context}.
+
+%% @doc Determines the time the asset was last modified
+-spec last_modified(wrq:reqdata(), #context{}) ->
+                           {undefined | calendar:datetime(),
+                            wrq:reqdata(), #context{}}.
+last_modified(ReqData, #context{filename=template}=Context) ->
+    {undefined, ReqData, Context};
+last_modified(ReqData, #context{fileinfo={ok, #file_info{mtime=MTime}}}=Context) ->
+    {MTime, ReqData, Context}.
+
+%% @doc Given a series of request tokens, normalize to priv dir file.
+-spec normalize_filepath(list()) -> list().
+normalize_filepath(Filepath) ->
+    {ok, App} = application:get_application(?MODULE),
+    filename:join([priv_dir(App), "www"] ++ Filepath).
+
+%% @doc Return a context which determines if we serve up the index or a
+%%      particular file
+-spec identify_resource(wrq:reqdata(), #context{}) ->
+    {boolean(), #context{}}.
+identify_resource(ReqData, #context{filename=undefined}=Context) ->
+    io:format("disp_path: ~p~n", [wrq:disp_path(ReqData)]),
+    case wrq:disp_path(ReqData) of
+        [] ->
+            Filename = normalize_filepath(["index.html"]),
+            FileInfo = file:read_file_info(Filename),
+            {true, Context#context{filename=Filename,
+                                   fileinfo=FileInfo}};
+        _ ->
+            Tokens = wrq:path_tokens(ReqData),
+            Filename = normalize_filepath(Tokens),
+            FileInfo = file:read_file_info(Filename),
+            {true, Context#context{filename=Filename,
+                                   fileinfo=FileInfo}}
+    end;
+identify_resource(_ReqData, Context) ->
+    {true, Context}.
+
+%% @doc If the file exists, allow it through, otherwise assume true if
+%%      they are asking for the application template.
+-spec resource_exists(wrq:reqdata(), #context{}) ->
+    {boolean(), wrq:reqdata(), #context{}}.
+resource_exists(ReqData, Context) ->
+    case identify_resource(ReqData, Context) of
+        {true, NewContext=#context{filename=template}} ->
+            {true, ReqData, NewContext};
+        {true, NewContext=#context{filename=Filename}} ->
+            case filelib:is_regular(Filename) of
+                true ->
+                    {true, ReqData, NewContext};
+                _ ->
+                    {false, ReqData, NewContext}
+            end
+    end.
+
+%% @doc Return the proper content type of the file, or default to
+%%      text/html.
+-spec content_types_provided(wrq:reqdata(), #context{}) ->
+    {list({list(), atom()}), wrq:reqdata(), #context{}}.
+content_types_provided(ReqData, Context) ->
+    case identify_resource(ReqData, Context) of
+        {true, NewContext=#context{filename=template}} ->
+            {[{"text/html", to_resource}], ReqData, NewContext};
+        {true, NewContext=#context{filename=Filename}} ->
+            MimeType = webmachine_util:guess_mime(Filename),
+            {[{MimeType, to_resource}], ReqData, NewContext};
+        {true, NewContext} ->
+            {[{"text/html", to_resource}], ReqData, NewContext}
+    end.
+
+%% @doc Return the resources content.
+-spec to_resource(wrq:reqdata(), #context{}) ->
+    {binary(), wrq:reqdata(), #context{}}.
+to_resource(ReqData, #context{filename=template,
+                              rendered=Content,
+                              token=Token}=Context) ->
+    {Content,
+     wrq:set_resp_header("Set-Cookie",
+                         "csrf_token="++Token++"; httponly", ReqData),
+     Context};
+to_resource(ReqData, #context{filename=Filename}=Context) ->
+    {ok, Source} = file:read_file(Filename),
+    {Source, ReqData, Context}.
+
+%% @doc Extract the priv dir for the application.
+-spec priv_dir(term()) -> list().
+priv_dir(Mod) ->
+    case code:priv_dir(Mod) of
+        {error, bad_name} ->
+            Ebin = filename:dirname(code:which(Mod)),
+            filename:join(filename:dirname(Ebin), "priv");
+        PrivDir ->
+            PrivDir
+    end.
